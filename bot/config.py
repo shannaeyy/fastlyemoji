3936115885af1b46655497c7ad1c0e3dbfe5671c
shannaeyy <SHANFIRE177@GMAIL.COM>
2025-07-30 17:19:32 +0100

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -3,6 +3,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-TELEGRAM_TOKEN = os.getenv('8473212500:AAFkyOPWatDegGbZ6sabSw9-__bjx-K5t4g')
+TELEGRAM_TOKEN = os.getenv('8473212500:AAEgo9Zz85zItQXwX8rKUslhm1ZqeQJ42iM')
 MONGODB_URI = os.getenv('mongodb+srv://shannaeyy:Binci1234112233@cluster0.zaimjvl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
 CHANNEL_URL = 'https://t.me/shannaeyy'
